5aec7713881a3e4664de5be69db1bde1af68804d
renaw <evan_20170613@163.com>
2017-09-01 16:33:04 +0800

    levenshtein

---
+++ Common/CacheHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+
+namespace Common
+{
+    /// <summary>
+    /// 缓存Helper
+    /// </summary>
+    public class CacheHelper
+    {
+        /// <summary>
+        ///     获取数据缓存
+        /// </summary>
+        /// <param name="cacheKey">键</param>
+        public static object GetCache(string cacheKey)
+        {
+            Cache objCache = HttpRuntime.Cache;
+            return objCache.Get(cacheKey);
+        }
+
+        /// <summary>
+        ///  设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey">Key值</param>
+        /// <param name="objObject">对象</param>
+        /// <param name="fileName">文件名</param>
+        /// <param name="timeOutHours">过期时间</param>
+        public static void SetCache(string cacheKey, object objObject, string fileName = null, int timeOutHours = 2)
+        {
+            Cache objCache = HttpRuntime.Cache;
+            CacheDependency cacheDependency = fileName == string.Empty ? null : new CacheDependency(fileName);
+            //objCache.Insert(cacheKey, objObject);
+            objCache.Insert(cacheKey, objObject, cacheDependency, Cache.NoAbsoluteExpiration,
+                TimeSpan.FromMinutes(timeOutHours));
+        }
+
+        /// <summary>
+        ///  设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey">Key值</param>
+        /// <param name="objObject">对象</param>
+        /// <param name="fileName">文件名</param>
+        /// <param name="timeOutHours">过期时间</param>
+        public static void SetCache(string cacheKey, object objObject, int timeOutMinute)
+        {
+            Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(cacheKey, objObject, null, Cache.NoAbsoluteExpiration,
+                TimeSpan.FromMinutes(timeOutMinute));
+        }
+
+        /// <summary>
+        /// 设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey">Key值></param>
+        /// <param name="objObject">对象</param>
+        /// <param name="Timeout">过期时间</param>
+        public static void SetCache(string cacheKey, object objObject, TimeSpan Timeout)
+        {
+            Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(cacheKey, objObject, null, DateTime.MaxValue, Timeout, CacheItemPriority.NotRemovable, null);
+        }
+
+        /// <summary>
+        ///     设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey">Key值</param>
+        /// <param name="objObject">对象</param>
+        /// <param name="absoluteExpiration">绝对过期</param>
+        /// <param name="slidingExpiration">滑动过期</param>
+        public static void SetCache(string cacheKey, object objObject, DateTime absoluteExpiration,
+            TimeSpan slidingExpiration)
+        {
+            Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(cacheKey, objObject, null, absoluteExpiration, slidingExpiration);
+        }
+
+        /// <summary>
+        ///   移除指定数据缓存
+        /// </summary>
+        /// <param name="cacheKey">Key值</param>
+        public static void RemoveCache(string cacheKey)
+        {
+            Cache cache = HttpRuntime.Cache;
+            cache.Remove(cacheKey);
+        }
+
+        /// <summary>
+        ///     移除全部缓存
+        /// </summary>
+        public static void RemoveAllCache()
+        {
+            Cache cache = HttpRuntime.Cache;
+            IDictionaryEnumerator cacheEnum = cache.GetEnumerator();
+            while (cacheEnum.MoveNext())
+            {
+                cache.Remove(cacheEnum.Key.ToString());
+            }
+        }
+    }
+}
---
+++ Common/CookieHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+
+namespace Common
+{
+    public class CookieHelper
+    {
+        /// <summary>
+        ///  新增Cookies
+        /// </summary>
+        /// <param name="Response">Response</param>
+        /// <param name="Cookieskey">Cookieskey</param>
+        /// <param name="Value">Value</param>
+        /// <param name="minutes">失效的分钟</param>
+        public static void AddCookies(HttpResponseBase Response, string Cookieskey, string Value, int minutes)
+        {
+            Response.Cookies[Cookieskey].Value = Value;
+            Response.Cookies[Cookieskey].Expires = DateTime.Now.AddMinutes(minutes);
+        }
+
+        public static void AddCookies(string Cookieskey, string Value, int minutes)
+        {
+            HttpContext.Current.Response.Cookies[Cookieskey].Value = Value;
+            HttpContext.Current.Response.Cookies[Cookieskey].Expires = DateTime.Now.AddMinutes(minutes);
+        }
+
+        /// <summary>
+        ///  根据Key获取值
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <param name="Cookieskey"></param>
+        /// <returns></returns>
+        public static string GetValueByCookieskey(HttpRequestBase Request, string Cookieskey)
+        {
+            string cookiesValue = string.Empty;
+            if (Request.Cookies[Cookieskey] != null)
+            {
+                cookiesValue = Request.Cookies[Cookieskey].Value;
+            }
+            return cookiesValue;
+        }
+
+        /// <summary>
+        ///  根据Key获取值
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <param name="Cookieskey"></param>
+        /// <returns></returns>
+        public static string GetValueByCookieskey(string Cookieskey)
+        {
+            string cookiesValue = string.Empty;
+            if (HttpContext.Current.Request.Cookies[Cookieskey] != null)
+            {
+                cookiesValue = HttpContext.Current.Request.Cookies[Cookieskey].Value;
+            }
+            return cookiesValue;
+        }
+
+        /// <summary>
+        ///  删除Cookies
+        /// </summary>
+        /// <param name="Request">Request</param>
+        /// <param name="Response">Response</param>
+        /// <param name="Cookieskey">Cookieskey</param>
+        public static void RemoveCookiesByCookieskey(HttpRequestBase Request, HttpResponseBase Response, string Cookieskey)
+        {
+            HttpCookie cookies = Request.Cookies[Cookieskey];
+            if (cookies != null)
+            {
+                cookies.Expires = DateTime.Today.AddDays(-1);
+                Response.Cookies.Add(cookies);
+            }
+        }
+
+        /// <summary>
+        ///  删除Cookies
+        /// </summary>
+        /// <param name="Request">Request</param>
+        /// <param name="Response">Response</param>
+        /// <param name="Cookieskey">Cookieskey</param>
+        public static void RemoveCookiesByCookieskey(string Cookieskey)
+        {
+            HttpCookie cookies = HttpContext.Current.Request.Cookies[Cookieskey];
+            if (cookies != null)
+            {
+                cookies.Expires = DateTime.Today.AddDays(-1);
+                HttpContext.Current.Response.Cookies.Add(cookies);
+            }
+        }
+    }
+}
