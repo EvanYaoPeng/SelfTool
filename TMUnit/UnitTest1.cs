33fbc7552a6e05666dd601ed17cd1844aff9b548
renaw <evan_20170613@163.com>
2017-10-24 10:08:59 +0800

    111

---
+++ TMUnit/UnitTest1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using Top.Api;
+using System.Net;
+using System.IO;
+
+namespace TMUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("cardId", "V10005588750");
+            parameters.Add("cardPwd", "113011");
+            parameters.Add("PID", "692855178");
+            parameters.Add("timestamp", "2017-09-14 00:15:15");
+            parameters.Add("app_key", "23568039");
+            parameters.Add("method", "alibaba.alihealth.healtharchive.examination.rich.validate");
+            string sign = SignTopRequest(parameters,"ac1927fc618297e557da42257a4a49cc", "md5");
+        //http://139.159.33.52:6000/TMService.ashx?
+            //    cardId=V10005588750&  FBD3FF75B9F313361592D38A3ED3B0FE
+        //        cardPwd=113011&
+        //        PID=692855178&
+        //        timestamp=2017-10-11+00:18:22&
+        //            sign=96C3648FACA87825305062932D64632E&app_key=23568039&method=alibaba.alihealth.healtharchive.examination.rich.validate
+            //parameters.Add("timestamp", "2017-10-09 06:19:19");
+            //parameters.Add("app_key", "23568039");
+            ////parameters.Add("packageId","1601920101");
+            //parameters.Add("method", "alibaba.alihealth.healtharchive.examination.rich.getschedules");
+            //{'institutionID':'9','timeTo':'2017-11-08','timeFrom':'2017-10-10','packageID':'1300189130'}
+            //Dictionary<string, string> parms = new Dictionary<string, string>();
+            //parms.Add("institutionID", "9");
+            //parms.Add("timeTo", "2017-11-08");
+            //parms.Add("timeFrom", "2017-10-10");
+            //parms.Add("packageID", "1300189130");// "{'institutionID':'9','timeTo':'2017-11-08','timeFrom':'2017-10-10','packageID':'1300189130'}"
+            //string sign = Sign(parameters,FastJSON.JSON.ToJSON(parms), "ac1927fc618297e557da42257a4a49cc", "md5");
+
+
+            Console.WriteLine(sign + "\r\n");
+
+        }
+
+        [TestMethod]
+        public void GetRequeest()
+        {
+            string str = "http://20.0.0.27:6010/TMService.ashx?PID=692855178&app_key=23568039&cardId=V10005588750&cardPwd=113011&method=alibaba.alihealth.healtharchive.examination.rich.validate&sign=5AC803EFE67D3B618B27F99C24288F6F&timestamp=2017-09-14 00:15:15";
+            HttpWebRequest myHttpWebRequest = null;
+            myHttpWebRequest = (HttpWebRequest)WebRequest.Create(str);
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.Method = "GET";
+            myHttpWebRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)";
+            //byte[] buf = System.Text.Encoding.UTF8.GetBytes(strParm);
+            //myHttpWebRequest.ContentLength = buf.Length;
+            //Stream rs = myHttpWebRequest.GetRequestStream();
+            //rs.Write(buf, 0, buf.Length);
+            //rs.Close();
+
+            HttpWebResponse response = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    string responseFromServer1 = reader.ReadToEnd();  //返回结果
+
+                }
+            }
+            response.Close();
+        }
+
+
+        [TestMethod]
+        public void Posttest()
+        {
+            HttpWebRequest myHttpWebRequest = null;
+            //myHttpWebRequest = (HttpWebRequest)WebRequest.Create("http://172.3.212.24:9000/TmallEticket.ashx");//接口地址
+            myHttpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:28176/TmallEticket.ashx");//接口地址
+       
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.Method = "POST";
+            myHttpWebRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)";
+            string paraUrlCoded = "charset=GBK&refundFee=450000&amount=2&bizType=3001&method=refundSuccess&itemTitle=瑞慈体检卡 职场高端套餐 全面男女报告表单中心上海北京成都深&smsTemplate=验证码$code.您已成功订购瑞慈体检旗舰店提供的瑞慈体检卡 职场高端套餐 全面男女报告表单中心上海北京成都深,有效期2017/09/18至2018/09/17,消费时请出示本短信以验证.如有疑问,请联系卖家.&sendStyle=0&validStart=2017-09-18 00:00:00&sign=30413499D85C56E5526DA9D880555C58&mobile=15958988207&mobileType=0&cancelNum=2&version=2.0&token=067c483107246bbab532c153c7195692&isPerfReq=false&itemId=537965740612&skuProperties=购买人数:1人&organizerNick=瑞慈体检旗舰店&organizerId=796659284&requestId=15087238730388311&validEnd=2018-09-17 23:59:59&outerId=67507568513054908698&timestamp=2017-10-23 09:57:53,038";
+            byte[] buf = System.Text.Encoding.UTF8.GetBytes(paraUrlCoded);
+            myHttpWebRequest.ContentLength = buf.Length;
+            Stream rs = myHttpWebRequest.GetRequestStream();
+            rs.Write(buf, 0, buf.Length);
+            rs.Close();
+
+            HttpWebResponse response = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    string responseFromServer1 = reader.ReadToEnd();  //返回结果
+
+                }
+            }
+            response.Close();
+        }
+
+        public static string SignTopRequest(IDictionary<string, string> parameters, string secret, string signMethod)
+        {
+            // 第一步：把字典按Key的字母顺序排序
+            IDictionary<string, string> sortedParams = new SortedDictionary<string, string>(parameters, StringComparer.Ordinal);
+            IEnumerator<KeyValuePair<string, string>> dem = sortedParams.GetEnumerator();
+
+            // 第二步：把所有参数名和参数值串在一起
+            StringBuilder query = new StringBuilder();
+            if (Constants.SIGN_METHOD_MD5.Equals(signMethod))
+            {
+                query.Append(secret);
+            }
+            while (dem.MoveNext())
+            {
+                string key = dem.Current.Key;
+                string value = dem.Current.Value;
+                if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
+                {
+                    query.Append(key).Append(value);
+                }
+            }
+
+            // 第三步：使用MD5/HMAC加密
+            byte[] bytes;
+            if (Constants.SIGN_METHOD_HMAC.Equals(signMethod))
+            {
+                HMACMD5 hmac = new HMACMD5(Encoding.UTF8.GetBytes(secret));
+                bytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(query.ToString()));
+            }
+            else
+            {
+                query.Append(secret);
+                MD5 md5 = MD5.Create();
+                bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(query.ToString()));
+            }
+
+            // 第四步：把二进制转化为大写的十六进制
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result.Append(bytes[i].ToString("X2"));
+            }
+
+            return result.ToString();
+        }
+
+        private static string Sign(IDictionary<string, string> parameters, string body, string secret, string charset)
+        {
+            IEnumerator<KeyValuePair<string, string>> em = parameters.GetEnumerator();
+
+            // 第1步：把所有参数名和参数值串在一起
+            StringBuilder query = new StringBuilder(secret);
+            while (em.MoveNext())
+            {
+                string key = em.Current.Key;
+                if (!Constants.SIGN.Equals(key))
+                {
+                    string value = em.Current.Value;
+                    query.Append(key).Append(value);
+                }
+            }
+            if (body != null)
+            {
+                query.Append(body);
+            }
+
+            query.Append(secret);
+
+            // 第2步：使用MD5加密
+            MD5 md5 = MD5.Create();
+            byte[] bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(query.ToString()));
+
+            // 第3步：把二进制转化为大写的十六进制
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result.Append(bytes[i].ToString("X2"));
+            }
+
+            return result.ToString();
+        }
+
+
+        public static string SignTopRequest(IDictionary<string, string> parameters,string body, string secret, string signMethod)
+        {
+            // 第一步：把字典按Key的字母顺序排序
+            IDictionary<string, string> sortedParams = new SortedDictionary<string, string>(parameters, StringComparer.Ordinal);
+            IEnumerator<KeyValuePair<string, string>> dem = sortedParams.GetEnumerator();
+
+            // 第二步：把所有参数名和参数值串在一起
+            StringBuilder query = new StringBuilder();
+            if (Constants.SIGN_METHOD_MD5.Equals(signMethod))
+            {
+                query.Append(secret);
+            }
+            while (dem.MoveNext())
+            {
+                string key = dem.Current.Key;
+                string value = dem.Current.Value;
+                if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
+                {
+                    query.Append(key).Append(value);
+                }
+            }
+            query.Append(body);
+
+            // 第三步：使用MD5/HMAC加密
+            byte[] bytes;
+            if (Constants.SIGN_METHOD_HMAC.Equals(signMethod))
+            {
+                HMACMD5 hmac = new HMACMD5(Encoding.UTF8.GetBytes(secret));
+                bytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(query.ToString()));
+            }
+            else
+            {
+                query.Append(secret);
+                MD5 md5 = MD5.Create();
+                bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(query.ToString()));
+            }
+
+            // 第四步：把二进制转化为大写的十六进制
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result.Append(bytes[i].ToString("X2"));
+            }
+
+            return result.ToString();
+        }
+
+     
+    }
+}
---
+++ XMLUnitTest/SqlHelper.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace XMLUnitTest
+{
+    public static class SqlHelper
+    {
+        private static readonly string sqlConnFRS = "Server=127.0.0.1;Database=HRInsurance;User Id=sa;Password=rich123456;";
+        //public static string SqlConnCIS
+        //{
+        //    get { return SqlHelper.sqlConnCIS; }
+        //}   
+
+        //public static string SqlConnOA
+        //{
+        //    get { return SqlHelper.sqlConnOA; }
+        //} 
+
+
+        public static string SqlConnFRS
+        {
+            get { return sqlConnFRS; }
+        }
+
+
+        public static DataRow ExecuteDataRow(string connectionString, CommandType commandType, string commandText, params SqlParameter[] parms)
+        {
+            DataSet ds = ExecuteDataset(connectionString, commandType, commandText, parms);
+            if (ds == null) return null;
+            if (ds.Tables.Count == 0) return null;
+            if (ds.Tables[0].Rows.Count == 0) return null;
+            return ds.Tables[0].Rows[0];
+        }
+
+        public static DataTable ExecuteDataTableByPage(string connectionString, CommandType commandType, string tableName, string fields, string orderby, string order, int startIndex, int endIndex, string where)
+        {
+            DataSet ds = null;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    cn.Open();
+                    StringBuilder commandText = new StringBuilder();
+                    commandText.Append("WITH RES_Data AS (SELECT ");
+                    if (!string.IsNullOrEmpty(fields))
+                        commandText.Append(fields);
+                    else
+                        commandText.Append("*");
+                    commandText.Append(",ROW_NUMBER() OVER (ORDER BY " + orderby);
+                    if (!string.IsNullOrEmpty(order))
+                        commandText.Append(" " + order);
+                    commandText.Append(") AS 'RowNumber' FROM " + tableName);
+                    if (!string.IsNullOrEmpty(where))
+                        commandText.Append(where);
+                    commandText.Append(")SELECT * FROM RES_Data WHERE RowNumber BETWEEN " + startIndex + " AND " + endIndex);
+                    ds = ExecuteDataset(cn, commandType, commandText.ToString(), null);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            if (ds == null) return null;
+            if (ds.Tables.Count == 0) return null;
+            return ds.Tables[0];
+        }
+
+        public static DataTable ExecuteDataTable(string connectionString, CommandType commandType, string commandText, params SqlParameter[] parms)
+        {
+            DataSet ds = ExecuteDataset(connectionString, commandType, commandText, parms);
+            if (ds == null) return null;
+            if (ds.Tables.Count == 0) return null;
+            return ds.Tables[0];
+        }
+
+        public static DataSet ExecuteDataset(string connectionString, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            DataSet ds = null;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    cn.Open();
+                    ds = ExecuteDataset(cn, commandType, commandText, commandParameters);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return ds;
+        }
+
+        private static DataSet ExecuteDataset(SqlConnection connection, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            //create a command and prepare it for execution
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandTimeout = 300 * 6;
+            PrepareCommand(cmd, connection, null, commandText, commandParameters);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.Parameters.Clear();
+            return ds;
+        }
+
+        private static string[] ExecuteDataTablePrimarykey(SqlConnection connection, CommandType commandType, string tableName)
+        {
+            string[] pKeys = null;
+            string commandText = "select top 1 * from " + tableName;
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandTimeout = 300;
+            PrepareCommand(cmd, connection, null, commandText, null);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.Parameters.Clear();
+            if (ds.Tables[0].PrimaryKey.Length > 0)
+            {
+                pKeys = new string[ds.Tables[0].PrimaryKey.Length];
+                for (int i = 0; i < pKeys.Length; i++)
+                {
+                    pKeys[i] = ds.Tables[0].PrimaryKey[i].ToString();
+                }
+            }
+            return pKeys;
+        }
+
+        public static string[] ExecuteDataTablePrimarykey(string connectionString, CommandType commandType, string tableName)
+        {
+            string[] pKeys = null;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    cn.Open();
+                    pKeys = ExecuteDataTablePrimarykey(cn, commandType, tableName);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return pKeys;
+        }
+
+        private static int ExecuteNonQuery(SqlTransaction trans, string cmdText, params SqlParameter[] paras)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandTimeout = 600;
+            PrepareCommand(cmd, trans.Connection, trans, cmdText, paras);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        public static int ExecuteNonQuery(string connectionString, CommandType commandType, string commandText, params SqlParameter[] parms)
+        {
+            int result = 0;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    cn.Open();
+                    //call the overload that takes a connection in place of the connection string
+                    result = ExecuteNonQuery(cn, commandType, commandText, parms);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            return result;
+        }
+
+        public static int ExecuteNonQuery(SqlConnection connection, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            //create a command and prepare it for execution
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandTimeout = 60 * 30;
+            PrepareCommand(cmd, connection, null, commandText, commandParameters);
+            int result = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return result;
+        }
+
+        public static void ExecuteNonQuery(string connectionString, List<string> lstCommandText)
+        {
+            if (lstCommandText.Count < 1)
+                return;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlTransaction trans = con.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (string commandText in lstCommandText)
+                        {
+                            if (string.IsNullOrWhiteSpace(commandText))
+                                continue;
+                            SqlCommand cmd = new SqlCommand(commandText, con, trans);
+                            cmd.ExecuteNonQuery();
+                        }
+                        trans.Commit();
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                    }
+                }
+            }
+        }
+
+        public static void ExecuteNonQuery(string connectionString, string[] lstCommandText)
+        {
+            if (lstCommandText.Length < 1)
+                return;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlTransaction trans = con.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (string commandText in lstCommandText)
+                        {
+                            if (string.IsNullOrWhiteSpace(commandText))
+                                continue;
+                            SqlCommand cmd = new SqlCommand(commandText, con, trans);
+                            cmd.ExecuteNonQuery();
+                        }
+                        trans.Commit();
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                    }
+                }
+            }
+        }
+
+        public static int ExecuteNonQueryInTransaction(string connectionString, string commandText)
+        {
+            int i = 0;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlTransaction trans = con.BeginTransaction())
+                {
+                    try
+                    {
+                        if (!string.IsNullOrWhiteSpace(commandText))
+                        {
+                            SqlCommand cmd = new SqlCommand(commandText, con, trans);
+                            cmd.CommandTimeout = 1800;
+                            i = cmd.ExecuteNonQuery();
+                            trans.Commit();
+                        }
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                    }
+                }
+            }
+            return i;
+        }
+
+        public static SqlDataReader ExecuteReader(string connectionString, string commandText, params SqlParameter[] commandParameters)
+        {
+            //return Sql.Data.SqlClient.SqlHelper.ExecuteReader(connectionString, commandText, commandParameters);
+            return ExecuteReader(connectionString, CommandType.Text, commandText, commandParameters);
+        }
+
+        public static SqlDataReader ExecuteReader(string connectionString, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            SqlDataReader reader = null;
+            SqlConnection cn = new SqlConnection(connectionString);
+            try
+            {
+                cn.Open();
+                reader = ExecuteReader(cn, commandType, commandText, commandParameters);
+            }
+            catch
+            {
+                cn.Close();
+                cn.Dispose();
+                return null;
+            }
+            return reader;
+        }
+
+        public static SqlDataReader ExecuteReader(SqlConnection connection, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandTimeout = 60;
+            PrepareCommand(cmd, connection, null, commandText, commandParameters);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            cmd.Parameters.Clear();
+            return dr;
+        }
+
+        public static object ExecuteScalar(SqlConnection connection, string commandText)
+        {
+            return ExecuteScalar(connection, commandText, null);
+        }
+        public static object ExecuteScalar(string connectionString, string commandText)
+        {
+            return ExecuteScalar(connectionString, commandText, null);
+        }
+        public static object ExecuteScalar(SqlConnection connection, string commandText, params SqlParameter[] commandParameters)
+        {
+            return ExecuteScalar(connection, CommandType.Text, commandText, commandParameters);
+        }
+        public static object ExecuteScalar(string connectionString, string commandText, params SqlParameter[] commandParameters)
+        {
+            return ExecuteScalar(connectionString, CommandType.Text, commandText, commandParameters);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="commandType"></param>
+        /// <param name="commandText"></param>
+        /// <param name="commandParameters"></param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string connectionString, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            object result = null;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    cn.Open();
+                    //call the overload that takes a connection in place of the connection string
+                    result = ExecuteScalar(cn, commandType, commandText, commandParameters);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return result;
+        }
+
+        public static object ExecuteScalar(SqlConnection connection, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandTimeout = 60;
+            PrepareCommand(cmd, connection, null, commandText, commandParameters);
+            object result = cmd.ExecuteScalar();
+            cmd.Parameters.Clear();
+            return result;
+        }
+
+        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, string cmdText, SqlParameter[] cmdParms)
+        {
+            //判断数据库连接状态
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+
+            //判断是否需要事物处理
+            if (trans != null)
+                cmd.Transaction = trans;
+            //      cmd.CommandType = cmdType;
+            if (cmdParms != null)
+            {
+                foreach (SqlParameter parm in cmdParms)
+                    cmd.Parameters.Add(parm);
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static int ExecuteProcedure(string name)
+        {
+            SqlConnection cn = new SqlConnection();
+            int result = 0;
+            try
+            {
+                cn.Open();
+                //call the overload that takes a connection in place of the connection string
+                result = ExecuteNonQuery(cn, CommandType.StoredProcedure, name, null);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+            return result;
+        }
+    }
+}
